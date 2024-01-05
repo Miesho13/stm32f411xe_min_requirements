9db89cd2620f0c54444e4bbc54d616c7ae48d08c
miesho <marcin.ryzewskii@gmail.com>
2024-01-05 10:53:26 +0100

    Implemented gpio_init function

--- src/driver/gpio.c
+++ src/driver/gpio.c
@@ -7,75 +7,14 @@
 
 #include "gpio.h"
 
-
-/// @brief Initializes the GPIOx peripheral according to the specified parameters 
-///        in the GPIO_InitStruct.
-/// @param GPIO_InitStruct pointer to a GPIO_InitTypeDef structure that contains 
-///        the configuration information for the specified GPIO peripheral. 
-void GPIO_Init(GPIO_InitTypeDef *GPIO_InitStruct) {
-    uint32_t pinpos = 0x00;
-    uint32_t pos = 0x00;
-    uint32_t currentpin = 0x00;
-
-    /* ------------------------- Configure the port pins ---------------- */
-    /*-- GPIO Mode Configuration --*/
-    for (pinpos = 0x00; pinpos < 0x10; pinpos++) {
-        pos = ((uint32_t)0x01) << pinpos;
-        /* Get the port pins position */
-        currentpin = (GPIO_InitStruct->GPIO_Pin) & pos;
-
-        if (currentpin == pos) {
-            // Pin Mode configuration 
-            // Configure Mode 
-            GPIO_InitStruct->GPIOx->MODER &= ~(GPIO_MODER_MODER0 << (pinpos * 2));
-            GPIO_InitStruct->GPIOx->MODER |= ((GPIO_InitStruct->GPIO_Mode) << (pinpos * 2));
-
-            // Pull-up Pull down resistor configuration 
-            GPIO_InitStruct->GPIOx->PUPDR &= ~(GPIO_PUPDR_PUPDR0 << ((uint16_t)pinpos * 2));
-            GPIO_InitStruct->GPIOx->PUPDR |= ((GPIO_InitStruct->GPIO_PuPd) << (pinpos * 2));
-           
-            //  Speed config 
-            GPIO_InitStruct->GPIOx->OSPEEDR &= ~(GPIO_OSPEEDER_OSPEEDR0 << (pinpos * 2));
-            GPIO_InitStruct->GPIOx->OSPEEDR |= ((GPIO_InitStruct->GPIO_Speed) << (pinpos * 2));
+void gpio_init(gpio_init_t hgpio) {
+    // pins =  (1 << 1) | (1 << 3) | (1 << 14)
+    for (uint16_t pin_ofs = 0; pin_ofs < 16; pin_ofs++) {
+        if ((1 << pin_ofs) == (hgpio.pins & (1 << pin_ofs))) {
+            hgpio.GPIOx->MODER |= (hgpio.mode << pin_ofs*2);
+            hgpio.GPIOx->OTYPER |= (hgpio.type << pin_ofs);
+            hgpio.GPIOx->OSPEEDR |= (hgpio.speed << pin_ofs*2);
+            hgpio.GPIOx->PUPDR |= (hgpio.pupd << pin_ofs*2);
         }
     }
-
-}
-
-
-/// @brief 
-/// @param GPIOx 
-/// @param GPIO_Pin 
-/// @return 
-GPIO_PinState GPIO_ReadPin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin) {
-    GPIO_PinState bitstatus = GPIO_PIN_RESET;
-
-    if ( (GPIOx->IDR & GPIO_Pin) != (uint32_t)GPIO_PIN_RESET ) {
-        bitstatus = GPIO_PIN_SET;
-    } else {
-        bitstatus = GPIO_PIN_RESET;
-    }
-
-    return bitstatus;
-}
-
-
-/// @brief Toggles the specified GPIO pin. 
-/// @param GPIOx  
-/// @param GPIO_Pin 
-/// @param PinState 
-void GPIO_WritePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState) {
-    if (PinState != GPIO_PIN_RESET) {
-        GPIOx->BSRR = GPIO_Pin;
-    } else {
-        GPIOx->BSRR = (uint32_t)GPIO_Pin << 16;
-    }
-}
-
-
-/// @brief 
-/// @param GPIOx 
-/// @param GPIO_Pin 
-void GPIO_TogglePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin) {
-    GPIOx->ODR ^= GPIO_Pin;
 }
--- src/driver/gpio.h
+++ src/driver/gpio.h
@@ -5,56 +5,47 @@
 #include "../cmsis/system_stm32f4xx.h"
 
 
-// MARCRO DEFINE START
-// Define GPIO Port/Pin A 
-#define GPIO_PORT_A GPIOA
 
-// Define GPIO Port/Pin B
-#define GPIO_PORT_B GPIOB
-
-// Define GPIO Port/Pin C
-#define GPIO_PORT_C GPIOC
-
-// Define GPIO Port/Pin D
-#define GPIO_PORT_D GPIOD 
-
-// Define GPIO Port/Pin E
-#define GPIO_PORT_E GPIOE
-
-// Define GPIO Port/Pin F
-#define GPIO_PORT_H GPIOH
-
-// MACRO GPIO END
 typedef enum {
-    GPIO_PIN_RESET = 0,
-    GPIO_PIN_SET
+    GPIO_MODE_INPUT = 0,
+    GPIO_MODE_OUTPUT,
+    GPIO_MODE_ALT,
+    GPIO_MODE_ANALOG,
 
-} GPIO_PinState;
+} GPIO_MODE;
+
+typedef enum {
+    GPIO_TYPE_PUSH_PULL = 0,
+    GPIO_TYPE_OPEN_DRAIN,
+
+} GPIO_TYPE;
+
+typedef enum {
+    GPIO_PUPD_NO = 0,
+    GPIO_PUPD_UP,
+    GPIO_PUPD_DOWN,
+
+} GPIO_PUPD;
+
+typedef enum {
+    GPIO_SPEED_LOW = 0,
+    GPIO_SPEED_MID,
+    GPIO_SPEED_FAST,
+    GPIO_SPEED_HIGH,
+
+} GPIO_SPEED;
 
 
 typedef struct {
-    GPIO_TypeDef* GPIOx;  // GPIO Port
-    uint32_t GPIO_Mode;   // GPIO Mode
-    uint32_t GPIO_Pull;   // GPIO Pull-Up/Pull-Down
-    uint32_t GPIO_Speed;  // GPIO Speed
-    uint32_t GPIO_PuPd;   // GPIO Pull-Up/Pull-Down
-    uint16_t GPIO_Pin;    // GPIO Pin
+    GPIO_TypeDef *GPIOx;
+    GPIO_MODE mode;
+    GPIO_SPEED speed;
+    GPIO_PUPD pupd;
+    uint16_t pins;
+    GPIO_TYPE type;
 
-} GPIO_InitTypeDef;
+} gpio_init_t;
 
-
-
-#define GPIO_MODE_INPUT        0x00000000U   /* Input Floating Mode                   */
-#define GPIO_MODE_OUTPUT_PP    0x00000001U   /* Output Push Pull Mode                 */
-#define GPIO_MODE_OUTPUT_OD    0x00000011U   /* Output Open Drain Mode                */
-#define GPIO_MODE_AF_PP        0x00000002U   /* Alternate Function Push Pull Mode     */
-#define GPIO_MODE_AF_OD        0x00000012U   /* Alternate Function Open Drain Mode    */
-#define GPIO_MODE_ANALOG       0x00000003U   /* Analog Mode                           */
-
-
-void GPIO_Init(GPIO_InitTypeDef *GPIO_InitStruct);
-GPIO_PinState GPIO_ReadPin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
-void GPIO_WritePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState);
-void GPIO_TogglePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
+void gpio_init(gpio_init_t hgpio);
 
 #endif // __GPIO_H__
