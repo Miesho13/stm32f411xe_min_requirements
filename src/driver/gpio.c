a7030a3961af1ce180cbce2dec27a443b98e3adb
miesho <marcin.ryzewskii@gmail.com>
2024-01-06 21:28:04 +0100

    Add enums for GPIO_PINS and GPIO_STATE in gpio.h and gpio.c. Implement gpio_read_state.

--- src/driver/gpio.c
+++ src/driver/gpio.c
@@ -6,7 +6,7 @@
 */
 
 #include "gpio.h"
-#include <assert.h>
+
 
 void gpio_init(gpio_init_t hgpio) {
     // pins =  (1 << 1) | (1 << 3) | (1 << 14)
@@ -21,11 +21,12 @@ void gpio_init(gpio_init_t hgpio) {
 }
 
 
-void gpio_togle(GPIO_TypeDef *hgpio, uint16_t pins) {
+void gpio_togle(GPIO_TypeDef *hgpio, GPIO_PINS pins) {
     hgpio->ODR ^= pins;
 }
 
-void gpio_write(GPIO_TypeDef *hgpio, uint32_t pins, uint8_t state) {
+
+void gpio_write(GPIO_TypeDef *hgpio, GPIO_PINS pins, GPIO_STATE state) {
     if (state == 1) {
         hgpio->BSRR |= pins;
     }
@@ -34,4 +35,85 @@ void gpio_write(GPIO_TypeDef *hgpio, uint32_t pins, uint8_t state) {
     }
 }
 
-void gpio_read();
+
+uint16_t gpio_read(GPIO_TypeDef hgpio, GPIO_PINS pins) {
+    return (hgpio.IDR & pins);
+}
+
+
+GPIO_STATE gpio_read_state(GPIO_TypeDef hgpio, GPIO_PINS pins) {
+    switch (pins) {
+        case GPIO_PIN00:
+            return (hgpio.IDR & GPIO_PIN00);
+            break;
+        
+        case GPIO_PIN01:
+            return (hgpio.IDR & GPIO_PIN01) >> 1;
+            break;
+
+        case GPIO_PIN02:
+            return (hgpio.IDR & GPIO_PIN02) >> 2;
+            break;
+
+        case GPIO_PIN03:
+            return (hgpio.IDR & GPIO_PIN03) >> 3;
+            break;
+        
+        case GPIO_PIN04:
+            return (hgpio.IDR & GPIO_PIN04) >> 4;
+            break; 
+        
+        case GPIO_PIN05:
+            return (hgpio.IDR & GPIO_PIN05) >> 5;
+            break;
+        
+        case GPIO_PIN06:
+            return (hgpio.IDR & GPIO_PIN06) >> 6;
+            break;
+        
+        case GPIO_PIN07:
+            return (hgpio.IDR & GPIO_PIN07) >> 7;
+            break;
+        
+        case GPIO_PIN08:
+            return (hgpio.IDR & GPIO_PIN08) >> 8;
+            break;
+        
+        case GPIO_PIN09:
+            return (hgpio.IDR & GPIO_PIN09) >> 9;
+            break;
+        
+        case GPIO_PIN10:
+            return (hgpio.IDR & GPIO_PIN10) >> 10;
+            break;
+        
+        case GPIO_PIN11:
+            return (hgpio.IDR & GPIO_PIN11) >> 11;
+            break;
+        
+        case GPIO_PIN12:
+            return (hgpio.IDR & GPIO_PIN12) >> 12;
+            break;
+        
+        case GPIO_PIN13:
+            return (hgpio.IDR & GPIO_PIN13) >> 13;
+            break;
+        
+        case GPIO_PIN14:
+            return (hgpio.IDR & GPIO_PIN14) >> 14;
+            break;
+        
+        case GPIO_PIN15:
+            return (hgpio.IDR & GPIO_PIN15) >> 15;
+            break;
+        
+        default:
+            return 0;
+            break;
+    }
+    return 0;
+}
+
+
+
+
--- src/driver/gpio.h
+++ src/driver/gpio.h
@@ -5,6 +5,31 @@
 #include "../cmsis/system_stm32f4xx.h"
 
 
+typedef enum {
+    GPIO_PIN00 = (1 << 0),
+    GPIO_PIN01 = (1 << 1),
+    GPIO_PIN02 = (1 << 2),
+    GPIO_PIN03 = (1 << 3),
+    GPIO_PIN04 = (1 << 4),
+    GPIO_PIN05 = (1 << 5),
+    GPIO_PIN06 = (1 << 6),
+    GPIO_PIN07 = (1 << 7),
+    GPIO_PIN08 = (1 << 8),
+    GPIO_PIN09 = (1 << 9),
+    GPIO_PIN10 = (1 << 10),
+    GPIO_PIN11 = (1 << 11),
+    GPIO_PIN12 = (1 << 12),
+    GPIO_PIN13 = (1 << 13),
+    GPIO_PIN14 = (1 << 14),
+    GPIO_PIN15 = (1 << 15),
+
+} GPIO_PINS;
+
+typedef enum {
+    GPIO_L,
+    GPIO_H,
+
+} GPIO_STATE;
 
 typedef enum {
     GPIO_MODE_INPUT = 0,
@@ -14,11 +39,13 @@ typedef enum {
 
 } GPIO_MODE;
 
+
 typedef enum {
     GPIO_TYPE_PUSH_PULL = 0,
     GPIO_TYPE_OPEN_DRAIN,
 
 } GPIO_TYPE;
+
 
 typedef enum {
     GPIO_PUPD_NO = 0,
@@ -26,6 +53,7 @@ typedef enum {
     GPIO_PUPD_DOWN,
 
 } GPIO_PUPD;
+
 
 typedef enum {
     GPIO_SPEED_LOW = 0,
@@ -47,8 +75,9 @@ typedef struct {
 } gpio_init_t;
 
 void gpio_init(gpio_init_t hgpio);
-void gpio_togle(GPIO_TypeDef *hgpio, uint16_t pins);
-void gpio_write(GPIO_TypeDef *hgpio, uint32_t pins, uint8_t state); 
-
+void gpio_togle(GPIO_TypeDef *hgpio, GPIO_PINS pins);
+void gpio_write(GPIO_TypeDef *hgpio, GPIO_PINS pins, GPIO_STATE state); 
+uint16_t gpio_read(GPIO_TypeDef hgpio, GPIO_PINS pins); 
+GPIO_STATE gpio_read_state(GPIO_TypeDef hgpio, GPIO_PINS pins);
 
 #endif // __GPIO_H__
