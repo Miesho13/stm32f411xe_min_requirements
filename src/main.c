8f6a9f582d078bebab1701ed706a967349774ff5
miesho <marcin.ryzewskii@gmail.com>
2024-01-14 18:37:06 +0100

    add symple test program

--- src/main.c
+++ src/main.c
@@ -5,37 +5,63 @@
   @date 11/2023
 */
 
-#include <stdlib.h>
+
+// platform
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
 
+// c4
 #include "cmsis/stm32f4xx.h"
 #include "cmsis/system_stm32f4xx.h"
 
+// driver
 #include "driver/rcc.h"
+#include "system/system.h"
+#include "driver/gpio.h"
 #include "driver/timers.h"
+#include "driver/usart.h"
+
+#include "c_util/tester.h"
 
 
-void system_init() {
-    // Cortex-M4F core configuration
-    (void)SystemInit();
-
-    // set main clock to 100MHz
-    (void)RCC_Init();
-
+void gpio_startup() {
+    RCC->AHB1ENR  |= RCC_AHB1ENR_GPIODEN; 
+    gpio_init_t gpio_out_d = {
+        .GPIOx =  GPIOD,
+        .mode = GPIO_MODE_OUTPUT,
+        .speed = GPIO_SPEED_LOW,
+        .type = GPIO_TYPE_PUSH_PULL,
+        .pins = GPIO_PIN12 | GPIO_PIN13 | GPIO_PIN14 | GPIO_PIN15,
+        .pupd = GPIO_PUPD_DOWN,
+    };
+    gpio_init(gpio_out_d);
 }
 
-void system_loop() { 
-
+void system_init() {
+    SystemInit();
+    rcc_init();
+    system_function_init();
+    gpio_startup();
+    
+    // DUMMY LOOP
+    for (volatile int i = 0; i < 100000; i++) { }
 }
 
 
 int main(int argc, char const *argv[]) {
     (void)argc;
     (void)argv;
-    
-    (void)system_init();
+
+    system_init();
+
+    // main loop
     while (true) {
-        (void)system_loop();
+        gpio_togle(GPIOD, GPIO_PIN15 | 
+                          GPIO_PIN14 |
+                          GPIO_PIN13 |
+                          GPIO_PIN12 );
+        system_delay_ms(1000);
     }
+
 }
