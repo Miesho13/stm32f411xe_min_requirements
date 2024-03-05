78ed2523c130e3fd936128c138443446fd2a2629
miesho <marcin.ryzewskii@gmail.com>
2024-03-05 11:26:26 +0100

    Refactor system_function_init to limit prescaler base frequency

--- src/system/system.c
+++ src/system/system.c
@@ -1,14 +1,16 @@
 #include "system.h"
 #include "../driver/timers.h"
 #include "../cmsis/stm32f4xx.h"
-
+#include "../hard_info.h"
 
 
 void system_function_init(void) {
-    // reserv tim3 for system_delay_ms
-    // div = ClockSRC / out -> out = 1000 
+    uint32_t psc_base = SystemCoreClock;
+    if (psc_base >= HARD_MAX_APB_FREQ) {
+        psc_base = HARD_MAX_APB_FREQ; 
+    }
     RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
-    TIM3->PSC = ((uint32_t)(SystemCoreClock / 1000)); 
+    TIM3->PSC = ((uint16_t)(psc_base / 1000)); 
     TIM3->ARR = 0xffff - 1;
     TIM3->CR1 |= TIM_CR1_CEN;
 }
@@ -16,5 +18,5 @@ void system_function_init(void) {
 // DELAY BASE ON TIM3 COUNTER
 void  system_delay_ms(uint32_t ms) {
     TIM3->CNT = 0;
-    while ((TIM3->CNT) <= ms) { }
+    while ((TIM3->CNT) < ms) { }
 }
