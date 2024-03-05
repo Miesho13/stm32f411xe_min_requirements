c7bcf602861fe65e412eebb118c329f390296d4b
miesho <marcin.ryzewskii@gmail.com>
2024-03-05 11:31:00 +0100

    Add gdb server logging to debug_target.sh script

--- util/debug_target.sh
+++ util/debug_target.sh
@@ -3,6 +3,8 @@
 
 TARGET="load build/stm32_f411xe_min_require.elf"
 
+run_gdb_server.sh > debug_service.log &
+
 arm-none-eabi-gdb -ex "target remote localhost:61234" \
                   -ex ${TARGET} \
                   -ex "break main"
