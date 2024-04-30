997ec2798e9455ac87158848893178b73e5ab46a
miesho <marcin.ryzewskii@gmail.com>
2024-04-30 18:23:05 +0200

    init header usart driver

---
+++ src/driver/usart.c
@@ -0,0 +1,10 @@
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "gpio.h"
+#include "usart.h"
+#include "../cmsis/stm32f4xx.h"
+
+
+
---
+++ src/driver/usart.h
@@ -0,0 +1,14 @@
+#ifndef __USART__
+#define __USART__
+
+
+#include <stdint.h>
+#include "../cmsis/stm32f4xx.h"
+#include "gpio.h"
+
+
+void usart2_open();
+void usart2_open();
+
+#endif
+
